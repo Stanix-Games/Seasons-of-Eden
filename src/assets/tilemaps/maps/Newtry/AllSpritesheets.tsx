09c96ee33c94d548eb1c1d0c683ebe1010b1b6ad
KrampusParty <ivanrcbusiness@gmail.com>
2018-01-31 23:44:11 +0100

    updated

--- src/assets/tilemaps/maps/Newtry/AllSpritesheets.tsx
+++ src/assets/tilemaps/maps/Newtry/AllSpritesheets.tsx
@@ -2,10 +2,12 @@
 <tileset name="AllSpritesheets" tilewidth="16" tileheight="16" tilecount="8192" columns="64">
  <image source="../../Renders/sprites.png" width="1024" height="2048"/>
  <tile id="6083">
-  <objectgroup draworder="index">
-   <object id="5" x="0" y="1.95652">
-    <polygon points="0,0 16.1739,0 18.3913,16.4348 -2.91304,15.087 -1.17391,-0.0869565"/>
-   </object>
-  </objectgroup>
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="6084">
+  <objectgroup draworder="index"/>
+ </tile>
+ <tile id="6085">
+  <objectgroup draworder="index"/>
  </tile>
 </tileset>
