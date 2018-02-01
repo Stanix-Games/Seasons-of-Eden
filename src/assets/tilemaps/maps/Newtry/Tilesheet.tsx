0d803a2477012c2708bde7e8d2fbf6ff459b0b2d
KrampusParty <ivanrcbusiness@gmail.com>
2018-02-01 21:02:00 +0100

    Push

---
+++ src/assets/tilemaps/maps/Newtry/Tilesheet.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tilesheet" tilewidth="16" tileheight="16" tilecount="8192" columns="64">
+ <image source="sprites.png" width="1024" height="2048"/>
+</tileset>
