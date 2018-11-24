bd9a70dae1d68077e02260227e367c0958d44ebc
Christopher Taylor <doctacloak@protonmail.com>
2018-11-24 09:58:43 -0600

    removed debug.log from update method in playermov.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -18,7 +18,6 @@ public class PlayerMovement : MonoBehaviour {
 	// Update is called once per frame
 	void Update () {
 		HandlePlayerMovement ();
-		Debug.Log (change);
 	}
 
 	private void HandlePlayerMovement () {
