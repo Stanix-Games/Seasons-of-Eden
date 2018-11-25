00ea098eafbb7f6a652470bae106b75820cf15ac
Christopher Taylor <doctacloak@protonmail.com>
2018-11-25 02:28:10 -0600

    FOV final code.

--- Assets/Scripts/FieldOfView.cs
+++ Assets/Scripts/FieldOfView.cs
@@ -2,21 +2,41 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FieldOfView : MonoBehaviour
-{
+public class FieldOfView : MonoBehaviour {
+    public float viewRadius = 5;
+    public float viewAngle = 135;
+    Collider2D[] playerInRadius;
+    public LayerMask obstacleMask, playerMask;
+    public List<Transform> visiblePlayer = new List<Transform> ();
 
-    // Start is called before the first frame update
-    void Start()
-    {
+    void FixedUpdate () {
+        FindVisiblePlayer ();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
+    void FindVisiblePlayer () {
+        playerInRadius = Physics2D.OverlapCircleAll (transform.position, viewRadius);
 
+        visiblePlayer.Clear ();
+
+        for (int i = 0; i < playerInRadius.Length; i++) {
+            Transform player = playerInRadius[i].transform;
+            Vector2 dirPlayer = new Vector2 (player.position.x - transform.position.x, player.position.y - transform.position.y);
+
+            if (Vector2.Angle (dirPlayer, transform.right) < viewAngle / 2) {
+                float distancePlayer = Vector2.Distance (transform.position, player.position);
+
+                if (!Physics2D.Raycast (transform.position, dirPlayer, distancePlayer, obstacleMask)) {
+                    if (player.name == "ToDetect") {
+                        visiblePlayer.Add (player);
+                    }
+                }
+            }
+        }
     }
-
-    private void FindMousePosition() {
-
+    public Vector2 DirFromAngle (float angle, bool isGlobal) {
+        if (!isGlobal) {
+            angle += transform.eulerAngles.z;
+        }
+        return new Vector2(Mathf.Cos(angle * Mathf.Deg2Rad), Mathf.Sin(angle * Mathf.Deg2Rad));
     }
 }
---
+++ Assets/Scripts/Wisp.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wisp : MonoBehaviour {
+    public GameObject target;
+    public float moveSpeed;
+    public float rotationSpeed;
+
+    Camera viewCamera;
+    public float angle { get; private set; }
+    public Vector3 mouseDelta { get; private set; }
+
+    void Start () {
+        viewCamera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        transform.position = Vector3.MoveTowards (transform.position, target.transform.position, moveSpeed * Time.deltaTime);
+        // Vector3 vectorToTarget = target.transform.position - transform.position;
+        // float angle = Mathf.Atan2 (vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+
+        mouseDelta = viewCamera.ScreenToWorldPoint (Input.mousePosition) - transform.position;
+        angle = Mathf.Atan2 (mouseDelta.y, mouseDelta.x) * Mathf.Rad2Deg;
+        transform.rotation = Quaternion.Slerp (transform.rotation, Quaternion.AngleAxis (angle, Vector3.forward), 1000);
+    }
+}
