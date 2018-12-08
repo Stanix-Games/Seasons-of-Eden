57cda682f611a8c305982a2aa045a7df83268303
Christopher Taylor <doctacloak@protonmail.com>
2018-12-08 17:38:46 -0600

    accepted merge of playermovement being redirected

---
+++ Assets/Scripts/StanixGames/Player/PlayerMovement.cs
@@ -0,0 +1,77 @@
+﻿using UnityEngine;
+
+namespace StanixGames.Player
+{
+	[
+		RequireComponent(typeof(Rigidbody2D)),
+		RequireComponent(typeof(Animator)),
+		RequireComponent(typeof(RotationFollowMouse))
+	]
+	public class PlayerMovement : MonoBehaviour {
+		public float movementSpeed;
+		private Rigidbody2D playerRigidBody;
+		private Vector3 change;
+		private Animator playerAnimator;
+		private RotationFollowMouse playerRotator;
+		private Vector2 mouseDelta;
+
+		// Use this for initialization
+		void Start () {
+			playerRigidBody = GetComponent<Rigidbody2D> ();
+			playerAnimator = GetComponent<Animator> ();
+		}
+
+		// Update is called once per frame
+		void Update () {
+			mouseDelta = GetComponent<RotationFollowMouse> ().mouseDelta;
+			HandlePlayerMovement ();
+			RotatePlayer ();
+		}
+
+		private void HandlePlayerMovement () {
+			UpdatePlayerAxis ();
+
+			if (IsMovementChange ()) {
+				MoveCharacter ();
+			} else { StopPlayerMovement (); }
+		}
+
+		private void ResetPlayerVector () {
+			change = Vector3.zero;
+		}
+		private void UpdatePlayerAxis () {
+			ResetPlayerVector ();
+			change.x = Input.GetAxisRaw ("Horizontal");
+			change.y = Input.GetAxisRaw ("Vertical");
+		}
+
+		private void MoveCharacter () {
+			playerRigidBody.MovePosition (
+				transform.position + change * movementSpeed * Time.deltaTime
+			);
+
+			StartPlayerMovement ();
+		}
+
+		private void StopPlayerMovement () {
+			playerAnimator.SetBool ("moving", false);
+		}
+
+		private void StartPlayerMovement () {
+			playerAnimator.SetBool ("moving", true);
+			playerAnimator.SetFloat ("moveX", change.x);
+			playerAnimator.SetFloat ("moveY", change.y);
+		}
+
+		private void RotatePlayer () {
+			if (!playerAnimator.GetBool ("moving")) {
+				playerAnimator.SetFloat ("moveX", mouseDelta.x);
+				playerAnimator.SetFloat ("moveY", mouseDelta.y);
+			}
+		}
+
+		private bool IsMovementChange () {
+			return change != Vector3.zero;
+		}
+	}
+}
